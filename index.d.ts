943ff4fd571f7c04a26ad957114a5d1a17fcdda7
naronA <otherthan.new@gmail.com>
2019-01-14 16:54:34 +0900

    微調整

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,13 @@
 declare module '*.vue' {
-  import Vue from 'vue'
-  export default Vue
+  import Vue from 'vue';
+  import bus from '~/plugins/eventBus.js';
+
+  module 'vue/types/vue' {
+    interface Vue {
+      $bus: any;
+      $nuxt: any;
+    }
+  }
+
+  export default Vue;
 }
--- nuxt.config.js
+++ nuxt.config.js
@@ -29,8 +29,7 @@ module.exports = {
   /*
   ** Plugins to load before mounting the App
   */
-  plugins: [],
-
+  plugins: [{ src: '~/plugins/eventBus.js', ssr: false }],
   /*
   ** Nuxt.js modules
   */
